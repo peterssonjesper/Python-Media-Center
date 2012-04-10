b46b353e99ca4c1dc3d81870a48c97cbe9347cad
Christian Svensson <blue@cmd.nu>
2012-04-10 23:30:05 +0200

    New GUI working with BG

--- player/gui/window.py
+++ player/gui/window.py
@@ -2,15 +2,23 @@ import kivy
 from kivy.app import App
 from kivy.config import Config
 from kivy.uix.widget import Widget
+from kivy.core.image import Image
+from kivy.graphics import RenderContext, Rectangle
 import pygame
 
 class RootWidget(Widget):
-  pass
+  def __init__(self, width, height):
+    Widget.__init__(self)
+    self.canvas = RenderContext()
+    self.bg = Image('player/gui/images/bg.png')
+    self.bg.texture.wrap = 'repeat'
+    self.bg.texture.uvsize = (width/512.0, height/512.0)
+    with self.canvas:
+      Rectangle(texture=self.bg.texture, size=(2,2), pos=(-1,-1))
 
 class Window(App):
   def __init__(self):
     App.__init__(self)
-    print dir(self)
     self.root = None;
     self.bg = None;
 
@@ -24,27 +32,8 @@ class Window(App):
     Config.set('graphics', 'width', str(disp.current_w))
     Config.set('graphics', 'height', str(disp.current_h))
     Config.set('graphics', 'fullscreen', 'true')
-    self.root = RootWidget()
+    self.root = RootWidget(disp.current_w, disp.current_h)
     return self.root
-
-  def setBackgroundImage(self):
-    self.bg = Canvas(self.root, width=self.resolution['width'], height=self.resolution['height'], bg="black", highlightthickness=0);
-    self.bg.img = ImageTk.PhotoImage(file="player/gui/images/bg.png");
-    self.bg.create_image(self.resolution['width']/2, self.resolution['height']/2, image=self.bg.img);
-  
-    self.content = Canvas(self.frame, width=self.resolution['width'], height=self.resolution['height']);
-    self.content.pack();
-    self.content.create_window(0, 0, anchor="nw", window=self.bg, width=self.resolution['width'], height=self.resolution['height']);
 
   def render(self):
     self.run()
-  
-  def register(self, windowId, fn):
-    pass
-    self.keyPressRegisters.append((windowId, fn))
-
-  #def get_bg(self):
-  # return self.bg
-
-  #def get_resolution(self):
-  # return self.resolution;
