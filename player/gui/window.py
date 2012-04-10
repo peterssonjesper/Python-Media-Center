a06833365a3c0122fcd55a35c0e58f9ee46a654c
Christian Svensson <blue@cmd.nu>
2012-04-11 00:17:13 +0200

    Backdrop w/ text

--- player/gui/window.py
+++ player/gui/window.py
@@ -1,20 +1,44 @@
 import kivy
 from kivy.app import App
 from kivy.config import Config
-from kivy.uix.widget import Widget
 from kivy.core.image import Image
 from kivy.graphics import RenderContext, Rectangle
 import pygame
 
-class RootWidget(Widget):
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+
+class BackdropWidget(Widget):
   def __init__(self, width, height):
     Widget.__init__(self)
+    self.width = width
+    self.height = height
+
+    # Load backdrop
     self.canvas = RenderContext()
     self.bg = Image('player/gui/images/bg.png')
     self.bg.texture.wrap = 'repeat'
-    self.bg.texture.uvsize = (width/512.0, height/512.0)
+    self.bg.texture.uvsize = (self.width/512.0, self.height/512.0)
     with self.canvas:
       Rectangle(texture=self.bg.texture, size=(2,2), pos=(-1,-1))
+
+
+
+class RootWidget(Widget):
+  def __init__(self, width, height):
+    Widget.__init__(self)
+    self.width = width
+    self.height = height
+
+    self.layout = FloatLayout(size=(width, height))
+    self.layout.add_widget(Label(text="Hello World!", pos_hint={'top': 1.2, 'x': -0.45}))
+    self.layout.add_widget(Label(text="Test"))
+
+    self.add_widget(BackdropWidget(width, height))
+    self.add_widget(self.layout)
 
 class Window(App):
   def __init__(self):
